c0f21776a984f8288fa4e69e55331a757fc33d54
Mithul_krs <mithakku30@gmail.com>
2026-03-03 23:54:24 +0530

    ingesion and preprocessing

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -15,3 +15,11 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
+if __name__ == "__main__":
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.error("An error occurred: Divide by Zero")
+        # 1. This triggers your custom formatting logic
+        # 2. It will print the detailed message to the console
+        raise CustomException(e,sys)
